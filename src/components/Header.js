d5cfa9e830d80f422bec4d6c9433650d3d21750e
changtailiu <liuchangtai1996@icloud.com>
2018-08-02 19:19:06 -0700

    Folder relocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg'
+import logo from '../assets/images/logo.svg'
 
 export class Header extends React.Component {
     render() {
         return (
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
+                <h1 className="App-title">Around</h1>
             </header>
         );
     }
