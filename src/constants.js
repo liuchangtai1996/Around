5fd8c5d43b00b6bb032f57b77ca1bfc22ba3a1a2
changtailiu <liuchangtai1996@icloud.com>
2018-09-07 02:13:35 -0700

    now supports video posts

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ export const GEO_OPTIONS = {
     enableHighAccuracy: true,
     maximumAge        : 3600000,
     timeout           : 27000
-}
+};
 export const POS_KEY = 'POS_KEY';
 export const AUTH_PREFIX = 'Bearer';
+export const LOC_SHAKE = 0.02;
